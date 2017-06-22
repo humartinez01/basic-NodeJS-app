2bf13026976847251e59e8cff19886776f40f949
Fredwin Rosales <fredwinrosales@gmail.com>
2017-06-22 10:17:51 -0400

    Conecta con mysql
    Se realiza conexión con mysql desde controlador

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,24 @@
  * Created by frosales on 22/06/2017.
  */
 
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "db_cmsproject"
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
 var list = null
 
 exports.index = function(req, res) {
-    
+
     list = [
         {'key': 'value-01'},
         {'key': 'value-02'}
