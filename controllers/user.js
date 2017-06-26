c90891747eede21f281e48c760c51d219af784a4
Fredwin Rosales <fredwinrosales@gmail.com>
2017-06-26 14:14:13 -0400

    CRUD user
    Se crea estructura CRUD para entidad usuario

--- controllers/user.js
+++ controllers/user.js
@@ -4,12 +4,47 @@
 
 var model = require('../models/user');
 
+/**
+ * User Index
+ * @param req
+ * @param res
+ */
 exports.index = function(req, res) {
     model.list(function (err, content) {
         if (err) {
             console.log(err);
         } else {
-            res.render('index', { title: 'User.index', content: content.toJSON() });
+            res.render('user/index', { title: 'User.index', content: content.toJSON() });
+        }
+    })
+}
+
+/**
+ * User Create
+ * @param req
+ * @param res
+ */
+exports.create = function(req, res) {
+    model.list(function (err, content) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('user/create', { title: 'User.create', content: content.toJSON() });
+        }
+    })
+}
+
+/**
+ * User Edit
+ * @param req
+ * @param res
+ */
+exports.edit = function(req, res) {
+    model.list(function (err, content) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render('user/edit', { title: 'User.edit', id: req.params.id });
         }
     })
 }
--- routes/users.js
+++ routes/users.js
@@ -4,20 +4,20 @@ var user = require('../controllers/user')
 
 router.get('/', user.index);
 
-router.get('/create', function(req, res) {
-  res.send('user.create');
-});
+router.get('/create', user.create);
 
 router.post('/store', function(req, res) {
-  res.send('user.create');
+  res.send('user.store');
 });
 
-router.get('/:id/edit', function(req, res) {
-  res.send('user.edit: ' + req.params.id);
-});
+router.get('/:id/edit', user.edit);
 
 router.put('/:id/update', function(req, res) {
-  res.send('user.edit: ' + req.params.id);
+  res.send('user.update: ' + req.params.id);
+});
+
+router.delete('/:id', function(req, res) {
+  res.send('user.delete: ' + req.params.id);
 });
 
 module.exports = router;
