06da79befe810ed64162707a9b2464578e9c1829
Fredwin Rosales <fredwinrosales@gmail.com>
2017-06-26 13:45:06 -0400

    Lectura de properties
    Se incorpora archivo de propiedades para configuración de base de datos.

--- bin/conn.js
+++ bin/conn.js
@@ -1,16 +1,26 @@
 /**
  * Created by frosales on 26/06/2017.
  */
+var PropertiesReader = require('properties-reader');
+var properties = PropertiesReader('.env');
+
+const client = properties.get('config_db.default.connection')
+const host = properties.get('config_db.default.host')
+const port = properties.get('config_db.default.port')
+const database = properties.get('config_db.default.database')
+const user = properties.get('config_db.default.username')
+const password = properties.get('config_db.default.password')
+const charset = properties.get('config_db.default.charset')
 
 const knex = require('knex')({
-    client: 'pg',
+    client: client,
     connection: {
-        host     : 'localhost',
-        user     : 'postgres',
-        port     : 5432,
-        password : '123',
-        database : 'db_postgres',
-        charset  : 'utf8'
+        host     : host,
+        user     : user,
+        port     : port,
+        password : password,
+        database : database,
+        charset  : charset
     }
 });
 
